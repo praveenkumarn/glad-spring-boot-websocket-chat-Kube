7f733df15cf5c3b9fab3ba278ec84ba36ebc364c
Administrator <admin@example.com>
2019-12-02 08:04:22 +0000

    Update WebSocketConfig.java

--- src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -17,6 +17,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
@@ -32,9 +37,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         */
     }
 
-     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
-    }
-
-}
+ }
